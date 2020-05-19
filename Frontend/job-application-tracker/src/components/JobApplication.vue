c2a98db50dfbfd3b8a0c66b828d0b7922b7abcef
WinnMoo <winnmoo@gmail.com>
2020-05-18 19:29:17 -0700

    Added frontend folder

---
+++ Frontend/job-application-tracker/src/components/JobApplication.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="JobApplications">
+    <v-data-table></v-data-table>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: JobApplication,
+  data () {
+    return {
+        jobApplications: [
+
+        ]
+    }
+  }
+}
+</script>
