01811939bbd24d0df2ff2537b475e88d70aa7843
WinnMoo <winnmoo@gmail.com>
2020-08-26 13:52:17 -0700

    Added reset password api to UserAccountController

--- Backend/ControllerLayer/Controllers/AccountController.cs
+++ Backend/ControllerLayer/Controllers/AccountController.cs
@@ -82,5 +82,64 @@ namespace ControllerLayer.Controllers
             throw new NotImplementedException();
         }
 
+        [HttpPost]
+        [Route("api/account/generateresetlink")]
+        public ActionResult GenerateResetLink(string emailAddress)
+        {
+            try
+            {
+                UserAccountManager _userAccountManager = new UserAccountManager(new MongoClient(MONGODB_CONNECTION_STRING));
+                return _userAccountManager.GenerateResetPassword(emailAddress);
+            } 
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/account/fetchsecurityquestions")]
+        public ActionResult FetchSecurityQuestions(string PasswordResetToken)
+        {
+            try
+            {
+                UserAccountManager _userAccountManager = new UserAccountManager(new MongoClient(MONGODB_CONNECTION_STRING));
+                return _userAccountManager.FetchSecurityQuestions(PasswordResetToken);
+            } 
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/account/checksecurityanswers")]
+        public ActionResult CheckSecurityAnswers(SecurityAnswerRequest request)
+        {
+            try
+            {
+                UserAccountManager _userAccountManager = new UserAccountManager(new MongoClient(MONGODB_CONNECTION_STRING));
+                return _userAccountManager.CheckSecurityAnswers(request);
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/account/resetpassword")]
+        public ActionResult ResetPassword(ResetPasswordRequest request)
+        {
+            try
+            {
+                UserAccountManager _userAccountManager = new UserAccountManager(new MongoClient(MONGODB_CONNECTION_STRING));
+                return _userAccountManager.ResetPassword(request);
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
