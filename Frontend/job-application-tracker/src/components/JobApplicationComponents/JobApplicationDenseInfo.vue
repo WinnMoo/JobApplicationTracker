2dc1c2b116cd00d5c29dc972d81f8a2d5ddf208a
WinnMoo <winnmoo@gmail.com>
2020-10-08 22:16:52 -0700

    Added card to show column info

---
+++ Frontend/job-application-tracker/src/components/JobApplicationComponents/JobApplicationDenseInfo.vue
@@ -0,0 +1,37 @@
+<template>
+<v-row dense>
+  <v-col>
+    <v-card style="position: sticky">
+    <v-row>
+      <v-col v-for="column in columns" :key="column.text">
+        {{column.text}}
+      </v-col>
+    </v-row>
+  </v-card>
+  </v-col>
+</v-row>
+  
+</template>
+
+<script>
+export default {
+  name: "JobApplicationDenseInfo",
+  data() {
+    return {
+      columns: [
+        {text: "Company Name"},
+        {text: "Job Title"},
+        {text: "Description"},
+        {text: "Date Applied"},
+        {text: "Location"},
+        {text: "Actions"},
+      ]
+    }
+  }
+};
+</script>
+<style>
+#fixed {
+  position: sticky;
+}
+</style>
